619ffa0e32e205e41890897746885674d7313257
Audrey Chen <Audrey@audreys-mbp.lan>
2022-03-24 14:15:36 -0700

    rich text editor

--- src/views/Share.js
+++ src/views/Share.js
@@ -1,7 +1,23 @@
-import { useState, useEffect } from 'react'
-
-export default function Share() {
-    return(
-        <p>nothing here yet!</p>
-    )
+import React, { useEffect, useState } from "react";
+import { Editor } from "react-draft-wysiwyg";
+import { EditorState } from "draft-js";
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+export default function App() {
+  const [editorState, setEditorState] = useState(() =>
+    EditorState.createEmpty()
+  );
+  useEffect(() => {
+    console.log(editorState);
+  }, [editorState]);
+  return (
+    <div>
+      <h1>Share Your Story!</h1>
+      <div style={{ border: "1px solid black", padding: '2px', minHeight: '400px' }}>
+        <Editor
+          editorState={editorState}
+          onEditorStateChange={setEditorState}
+        />
+      </div>
+    </div>
+  );
 }
