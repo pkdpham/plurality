4923d118f3e68a754730ca48a073898e12286181
Audrey Chen <Audrey@audreys-mbp.lan>
2022-03-24 14:20:02 -0700

    share story

--- src/views/Share.js
+++ src/views/Share.js
@@ -12,6 +12,8 @@ export default function App() {
   return (
     <div>
       <h1>Share Your Story!</h1>
+      <h2>Share about the impacts that United States politics has had on you.</h2>
+      <h3>Categorize your story by choosing a topic in the dropdown menu below.</h3>
       <div style={{ border: "1px solid black", padding: '2px', minHeight: '400px' }}>
         <Editor
           editorState={editorState}
