076d65cf1d230ce7541b4254e6cf535bcf72972b
Audrey Chen <Audrey@Audreys-MacBook-Pro.local>
2022-04-06 13:25:52 -0700

    dropdown menu

--- src/views/Share.js
+++ src/views/Share.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { Editor } from "react-draft-wysiwyg";
 import { EditorState } from "draft-js";
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 export default function App() {
   const [editorState, setEditorState] = useState(() =>
@@ -14,6 +16,13 @@ export default function App() {
       <h1>Share Your Story!</h1>
       <h2>Share about the impacts that United States politics has had on you.</h2>
       <h3>Categorize your story by choosing a topic in the dropdown menu below.</h3>
+      <DropdownButton id="dropdown-basic-button" title="Topic">
+        <Dropdown.Item href="#/action-1">COVID-19</Dropdown.Item>
+        <Dropdown.Item href="#/action-2">Immigration</Dropdown.Item>
+        <Dropdown.Item href="#/action-3">Abortion</Dropdown.Item>
+        <Dropdown.Item href="#/action-3">Economics</Dropdown.Item>
+        <Dropdown.Item href="#/action-3">Healthcare</Dropdown.Item>
+      </DropdownButton>
       <div style={{ border: "1px solid black", padding: '2px', minHeight: '400px' }}>
         <Editor
           editorState={editorState}
@@ -21,7 +30,7 @@ export default function App() {
         />
       </div>
       <button
-          className="button block primary"
+          className="buttonNav items"
           onClick={() => App({ editorState })}
         >
           {'Publish'}
