630afe6127c43bb07738225b95daac940d4a28b0
Paul Pham <paulkpham26@gmail.com>
2022-04-24 20:03:41 -0700

    add share page styling and responsive topic dropdown

--- src/views/Share.js
+++ src/views/Share.js
@@ -9,34 +9,45 @@ export default function App() {
   const [editorState, setEditorState] = useState(() =>
     EditorState.createEmpty()
   );
+
+  const [value, setValue] = useState('Topic');
+
   useEffect(() => {
     console.log(editorState);
   }, [editorState]);
+
+  const handleSelect = (e) => {
+    setValue(e)
+  }
+
   return (
-    <div>
+    <>
       <b><h1>Share Your Story</h1></b>
       <h2>Share about the impacts that United States politics has had on you.</h2>
       <h3>Categorize your story by choosing a topic in the dropdown menu below.</h3>
-      <DropdownButton id="dropdown-basic-button" title="Topic">
-        <Dropdown.Item href="#/action-1">COVID-19</Dropdown.Item>
-        <Dropdown.Item href="#/action-2">Immigration</Dropdown.Item>
-        <Dropdown.Item href="#/action-3">Abortion</Dropdown.Item>
-        <Dropdown.Item href="#/action-3">Economics</Dropdown.Item>
-        <Dropdown.Item href="#/action-3">Healthcare</Dropdown.Item>
-      </DropdownButton>
+      <div style={{ paddingTop: '1em', paddingBottom: '1em' }}>
+        <DropdownButton id="dropdown-basic-button" title={value} onSelect={handleSelect}>
+          <Dropdown.Item eventKey="COVID-19">COVID-19</Dropdown.Item>
+          <Dropdown.Item eventKey="Immigration">Immigration</Dropdown.Item>
+          <Dropdown.Item eventKey="Reproductive Rights">Reproductive Rights</Dropdown.Item>
+          <Dropdown.Item eventKey="Economics">Economics</Dropdown.Item>
+          <Dropdown.Item eventKey="Healthcare">Healthcare</Dropdown.Item>
+        </DropdownButton>
+      </div>
       <div style={{ border: "1px solid black", padding: '2px', minHeight: '400px' }}>
         <Editor
           editorState={editorState}
           onEditorStateChange={setEditorState}
         />
       </div>
-      <div className="buttonNav">
+      <div className="buttonNav" style={{ paddingTop: '1em' }}>
         <button
           onClick={() => App({ editorState })}
+          style={{ border: 'none' }}
         >
           {'Submit'}
         </button>
       </div>
-    </div>
+    </>
   );
 }
