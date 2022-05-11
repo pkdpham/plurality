e16175ed5fc65591e32a4f69724c9af0dca32f4a
Paul Pham <paulkpham26@gmail.com>
2022-05-11 12:43:40 -0700

    idk break it

--- src/components/StoryCard.js
+++ src/components/StoryCard.js
@@ -4,16 +4,31 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
-
+import { supabase } from "../supabaseClient"
 import { Link } from "react-router-dom";
 
 export default function StoryCard(props) {
+    let id = props.id;
+    // TODO: with props, get first 10 words of story to preview in card
+    
+    const dataRequest = async (id) => {
+        try {
+            let { data: stories, error } = await supabase
+                .from('stories')
+                .select('title, content') // TODO: get author from profiles table
+                .eq('id', id)
+
+            console.log(stories)
+        } catch (error) {
+            // console.log(error.message)
+        }
+    }
 
     const card = (
         <>
             <CardContent>
                 <Typography variant="h5" component="div">
-                    This is a title!
+                    {id}
                 </Typography>
                 <Typography variant="body2">
                     This is the card body text.
@@ -27,8 +42,8 @@ export default function StoryCard(props) {
 
     return(
         <>
-            <Link to={`/story/${props.id}`}>
-                <Box sx={{ minWidth: 275 }}>
+            <Link to={`/story/${id}`}>
+                <Box sx={{ minWidth: 275 }} onClick={dataRequest}>
                     <Card variant="outlined">{card}</Card>
                 </Box> 
             </Link>
